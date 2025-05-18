85888d9232fc4cd4750e901827edbd1c5276b900
Michael Bolden <103343331+MiLkEE44@users.noreply.github.com>
2025-05-17 20:03:26 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+def is_leap_year(year):
+
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        return True
+    else:
+        return False
+
+leap = is_leap_year(2024)
+print(leap)
