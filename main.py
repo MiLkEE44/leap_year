b3334f757418becc69e5e73c8fe1157f493fc18d
Michael Bolden <103343331+MiLkEE44@users.noreply.github.com>
2025-05-17 20:09:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+# The math to check if it's a leap year or not 
+# Was taken from a Google search 
+# This function returns True or False depending on the given year 
+# Being a Leap year or not. 
 def is_leap_year(year):
 
     if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
